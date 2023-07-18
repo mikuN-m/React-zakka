f58c974c92bd6814b2a1d4463b9e7ba36bb05f73
miku <nunon4msmrn@icloud.com>
2023-07-18 22:37:11 +0900

    🔧 環境構築

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './assets/style.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='content'>
+        <h1>hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
