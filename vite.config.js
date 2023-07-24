8d2ce2b32320f3250788364fceef48c978b4c11f
miku <nunon4msmrn@icloud.com>
2023-07-24 12:38:34 +0900

    デプロイ準備

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES 
+        ? "React-zakka"
+        : "./", 
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        dir: 'docs'
+      }
+    }
+  },
   plugins: [react()],
 })
